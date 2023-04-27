0917554f0c920f428fa7ccc5d011b823fee58211
Ronellyson <ronellysonjulio@gmail.com>
2023-04-27 11:33:26 -0300

    Add initial version of thread implementation

--- script/script.py
+++ script/script.py
@@ -1,23 +1,29 @@
 import subprocess
 import csv
+import threading
 
 qtTestes = int(input('Digite a quantidade de testes que serão realizados: '))
 
 def simularServico(taxaChegadaMedia, tempoServicoMedio, numServidores, tempoObservacao):
-    output = subprocess.run(
-    [
-        "java",
-        "-cp",
-        "../bin;../lib\*",
-        "ServidorWeb",
-        str(taxaChegadaMedia),
-        str(tempoServicoMedio),
-        str(numServidores),
-        str(tempoObservacao)
-    ], capture_output=True, text=True)
-    return output
+    # Definir a função da simulação
+    def simular():
+        subprocess.run(
+        [
+            "java",
+            "-cp",
+            "../bin;../lib\*",
+            "ServidorWeb",
+            str(taxaChegadaMedia),
+            str(tempoServicoMedio),
+            str(numServidores),
+            str(tempoObservacao)
+        ])
+    
+    # Criar uma nova thread para executar a simulação
+    thread = threading.Thread(target=simular)
+    thread.start()
+    return thread
 
-results = []
 simulacoes = [
     {
         'taxaChegadaMedia': taxaChegadaMedia,
@@ -26,22 +32,21 @@ simulacoes = [
         'tempoObservacao': 30
     } for taxaChegadaMedia in range(1, qtTestes+1)]
 
+threads = []
 for i in range(0, len(simulacoes)):
-    results.append(simularServico(
+    threads.append(simularServico(
         simulacoes[i]['taxaChegadaMedia'],
         simulacoes[i]['tempoServicoMedio'],
         simulacoes[i]['numServidores'],
-        simulacoes[i]['tempoObservacao']).stdout)
+        simulacoes[i]['tempoObservacao']))
 
-# Salvar resultados em um arquivo CSV
-with open('resultados.csv', mode='w', newline='', encoding='utf-8') as arquivo_csv:
-    writer = csv.writer(arquivo_csv)
-    # escrever o cabeçalho
-    writer.writerow(['Taxa de chegada média', 'Tempo de serviço médio', 'Número de servidores', 'Requisições submetidas', 'Requisições concluídas', 'Tempo médio de resposta', 'Tamanho médio da fila'])
-    # escrever cada linha de resultados
-    for result in results:
-        # remover o cabeçalho do resultado antes de escrever no arquivo CSV
-        linhas_resultado = result.strip().split('\n')[1:]
-        for linha in linhas_resultado:
-            colunas = linha.split()
-            writer.writerow(colunas)
+resultados = [thread.join() for thread in threads]
+print(resultados)
+
+# # Salvar resultados em um arquivo CSV
+# with open('resultados.csv', mode='w', newline='', encoding='utf-8') as arquivo_csv:
+#     writer = csv.writer(arquivo_csv)
+#     # escrever o cabeçalho
+#     writer.writerow(['Taxa de chegada média', 'Tempo de serviço médio', 'Número de servidores', 'Requisições submetidas', 'Requisições concluídas', 'Tempo médio de resposta', 'Tamanho médio da fila'])
+#     # escrever resultados na tabela
+#     writer.writerow(threads)
